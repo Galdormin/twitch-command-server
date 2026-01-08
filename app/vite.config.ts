05847f03fb129461a4f8f113379924fe715c157e
Galdormin <theoplenet@gmail.com>
2026-01-08 12:25:36 +0100

    add docker + pocketbase

---
+++ app/vite.config.ts
@@ -0,0 +1,17 @@
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [tailwindcss(), sveltekit()],
+    server: {
+        port: 3000,
+        host: true,
+        watch: {
+            usePolling: true,
+        },
+        hmr: {
+            host: 'localhost',
+        },
+    }
+});
